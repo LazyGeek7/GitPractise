06dcc50e3629750528d0a1cb7104c582950c0f4b
LazyGeek7 <utesthebbar@gmail.com>
2021-10-11 14:25:15 +0200

    eclipse comment

--- src/gitdemo/Student.java
+++ src/gitdemo/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//demo comment
+		//eclipse comment
 		
 		System.out.println("Its a sample Student class");
 		System.out.println("Its created for practise");
