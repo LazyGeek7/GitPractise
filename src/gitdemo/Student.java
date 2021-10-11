45fad9ca1894a6350eb92d1d3504cbb17a1e7be3
LazyGeek7 <92102215+LazyGeek7@users.noreply.github.com>
2021-10-11 14:01:12 +0200

    demo comment added

--- src/gitdemo/Student.java
+++ src/gitdemo/Student.java
@@ -4,6 +4,7 @@ public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//demo comment
 		
 		System.out.println("Its a sample Student class");
 		System.out.println("Its created for practise");
